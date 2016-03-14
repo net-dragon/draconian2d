20ed0d65e484fcfeccac92a33d6347f634abf32b
31emanual <mapurefoy@gmail.com>
2016-03-14 03:16:00 -0400

    adds a new attack script
    So far it can tell if you're charging, doesn't dash yet.

--- Draconian 2D/Assets/Scripts/PlayerAttack.cs
+++ Draconian 2D/Assets/Scripts/PlayerAttack.cs
@@ -1,15 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerAttack : MonoBehaviour {
+public class PlayerAttack : MonoBehaviour 
+{
+	private bool chargingDash;
+	public float chargeStart;
+	public float chargeCounter;
+
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () 
+	{
+		chargingDash = false;
+		chargeCounter = 0f;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void Update () 
+	{
+	 
+	}
+	private void FixedUpdate()
+	{
+		if (Input.GetButton ("Fire1")) 
+		{
+		
+			chargeCounter++;
+		
+			if (chargeCounter >= chargeStart)
+			
+				chargingDash = true;
+		}
+
+
+		if (Input.GetButtonUp ("Fire1")) 
+		{				
+			if (chargeCounter >= 100)
+						
+				StartCoroutine(DashRoutine());
+			   
+		}
+
+		
+
+		chargeCounter = 0f;
+	}
+
+	IEnumerator DashRoutine()
+	{
+		
 	}
 }

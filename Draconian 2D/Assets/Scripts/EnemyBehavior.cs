55029ab952639bf1ced4ea6768cff1176365ef68
josh65536 <josh4820@gmail.com>
2016-03-14 21:08:12 -0400

    Adds some features to the enemies
    Includes the bullets hitting the player causing a Game Over, a suspicion
    line of sight, and a greater line of sight when the enemy sees the
    player.

--- Draconian 2D/Assets/Scripts/EnemyBehavior.cs
+++ Draconian 2D/Assets/Scripts/EnemyBehavior.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class EnemyBehavior : MonoBehaviour {
 
-	public GameObject player;
+	private GameObject player;
 	public GameObject bullet;
 
-	public float coneAngle; //the angle from forward to the edge of the "cone" in degrees
-	public float coneLength;
+	public float normalConeAngle;
+	public float normalConeLength;
+
+	public float suspiciousConeAngle;
+	public float suspiciousConeLength;
+
+	public float expandedConeAngle;
+	public float expandedConeLength;
+
+	private float coneAngle; //the angle from forward to the edge of the "cone" in degrees
+	private float coneLength;
 
 	public uint shotDelay; //in frames
 	private uint shotCounter = 0;
@@ -16,7 +25,10 @@ public class EnemyBehavior : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		player = GameObject.FindGameObjectWithTag ("Player");
+
+		coneAngle = normalConeAngle;
+		coneLength = normalConeLength;
 	}
 	
 	// Update is called once per frame
@@ -26,10 +38,15 @@ public class EnemyBehavior : MonoBehaviour {
 
 	void FixedUpdate () {
 		float rad = coneAngle * 2*Mathf.PI / 360.0f;
+		float sRad = suspiciousConeAngle * 2*Mathf.PI / 360.0f;
 
 		Debug.DrawLine (transform.position, transform.position + Vector3.right * direction * coneLength);
 		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (rad) + Vector3.up * Mathf.Sin (rad)) * direction * coneLength);
 		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (rad) - Vector3.up * Mathf.Sin (rad)) * direction * coneLength);
+
+		Debug.DrawLine (transform.position, transform.position + Vector3.right * direction * suspiciousConeLength, Color.green);
+		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (sRad) + Vector3.up * Mathf.Sin (sRad)) * direction * suspiciousConeLength, Color.green);
+		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (sRad) - Vector3.up * Mathf.Sin (sRad)) * direction * suspiciousConeLength, Color.green);
 
 		Vector2 lookVector = player.transform.position - transform.position;
 
@@ -41,16 +58,32 @@ public class EnemyBehavior : MonoBehaviour {
 			GetComponent<SpriteRenderer> ().flipX = false;
 
 		//Shoot a bullet if it's looking at the player
-		if (lookVector.magnitude <= coneLength && //close enough
-		    Vector2.Dot ((Vector2.right * direction), lookVector.normalized) >= Mathf.Cos (rad)) {//is looking at the player
+		if (IsPlayerInsideCone(lookVector, rad, coneLength)) {//is looking at the player
 
 			if (shotCounter == 0) {
-				Instantiate(bullet, transform.position, Quaternion.FromToRotation(Vector2.right, lookVector));
+				Instantiate (bullet, transform.position, Quaternion.FromToRotation (Vector2.right, lookVector));
 				shotCounter = shotDelay;
+				transform.FindChild ("Enemy Suspicion").gameObject.SetActive (false);
 			}
+
+			coneAngle = expandedConeAngle;
+			coneLength = expandedConeLength;
+
+		} else {
+			
+			coneAngle = normalConeAngle;
+			coneLength = normalConeLength;
+
+			transform.FindChild ("Enemy Suspicion").gameObject.SetActive (IsPlayerInsideCone (lookVector, sRad, suspiciousConeLength));
 		}
 
 		if(shotCounter > 0)
 			--shotCounter;
+	}
+
+	bool IsPlayerInsideCone(Vector2 lookVector, float coneAngleRad, float coneLen)
+	{
+		return lookVector.magnitude <= coneLen && //close enough
+			Vector2.Dot ((Vector2.right * direction), lookVector.normalized) >= Mathf.Cos (coneAngleRad);
 	}
 }
--- Draconian 2D/Assets/Scripts/EnemyBullet.cs
+++ Draconian 2D/Assets/Scripts/EnemyBullet.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class EnemyBullet : MonoBehaviour {
@@ -12,7 +13,7 @@ public class EnemyBullet : MonoBehaviour {
 	void Start () {
 		lifeCounter = lifetime;
 
-		Rigidbody rb = GetComponent<Rigidbody> ();
+		Rigidbody2D rb = GetComponent<Rigidbody2D> ();
 		rb.velocity = transform.right * speed;
 	}
 	
@@ -27,5 +28,13 @@ public class EnemyBullet : MonoBehaviour {
 		Debug.Log ("Life Counter: " + lifeCounter.ToString());
 		if (lifeCounter == 0)
 			Destroy (gameObject);
+	}
+
+	void OnCollisionEnter2D(Collision2D clsn) {
+		if (clsn.collider.CompareTag ("Player")) {
+			clsn.gameObject.SetActive (false);
+
+			GameObject.Find ("Canvas/Message").GetComponent<Text> ().text = "Game Over!";
+		} 
 	}
 }

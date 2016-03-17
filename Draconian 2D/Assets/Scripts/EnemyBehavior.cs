f9aa38de6c9279593a9f35c8b7bf28f3840ce87f
31emanual <mapurefoy@gmail.com>
2016-03-17 17:42:05 -0400

    add UI elements

--- Draconian 2D/Assets/Scripts/EnemyBehavior.cs
+++ Draconian 2D/Assets/Scripts/EnemyBehavior.cs
@@ -26,7 +26,12 @@ public class EnemyBehavior : MonoBehaviour {
 	public uint shotDelay; //in frames
 	private uint shotCounter = 0;
 
+	public uint dashHitTime = 120;
+	private uint dashHitTimer = 0;
+
 	public int direction = -1; //-1 is left, 1 is right
+
+	private PlayerAttack playerAttack;
 
 	// Use this for initialization
 	void Start () {
@@ -34,6 +39,8 @@ public class EnemyBehavior : MonoBehaviour {
 
 		coneAngle = normalConeAngle;
 		coneLength = normalConeLength;
+
+		playerAttack = player.GetComponent<PlayerAttack> ();
 	}
 	
 	// Update is called once per frame
@@ -53,54 +60,60 @@ public class EnemyBehavior : MonoBehaviour {
 		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (sRad) + Vector3.up * Mathf.Sin (sRad)) * direction * suspiciousConeLength, Color.green);
 		Debug.DrawLine (transform.position, transform.position + (Vector3.right * Mathf.Cos (sRad) - Vector3.up * Mathf.Sin (sRad)) * direction * suspiciousConeLength, Color.green);
 
-		Vector2 lookVector = player.transform.position - transform.position;
+		if (dashHitTimer == 0) {
+			Vector2 lookVector = player.transform.position - transform.position;
 
-		//Get the proper direction
-		direction = lookVector.x == 0 ? direction : lookVector.x > 0 ? 1 : -1;
-		if (direction == 1)
-			GetComponent<SpriteRenderer> ().flipX = true;
-		else
-			GetComponent<SpriteRenderer> ().flipX = false;
+			//Get the proper direction
+			if (coneAngle == expandedConeAngle)
+				direction = lookVector.x == 0 ? direction : lookVector.x > 0 ? 1 : -1;
+			if (direction == 1)
+				GetComponent<SpriteRenderer> ().flipX = true;
+			else
+				GetComponent<SpriteRenderer> ().flipX = false;
 
-        if(shotStartTimer != 0)
-        {
-            ++shotStartTimer;
-            if(shotStartTimer == shotStartDelay)
-            {
-                shotStartTimer = 0;
-                Instantiate(bullet, transform.position, Quaternion.FromToRotation(Vector2.right, lookVector));
-                shotCounter = shotDelay;
-            }
-        }
-
-		//Shoot a bullet if it's looking at the player
-		if (IsPlayerInsideCone(lookVector, rad, coneLength)) {//is looking at the player
-
-			if (shotCounter == 0) {
-				
-                if(shotStartTimer == 0)
-                    ++shotStartTimer;
-				transform.FindChild ("Enemy Suspicion").gameObject.SetActive (false);
+			if (shotStartTimer != 0) {
+				++shotStartTimer;
+				if (shotStartTimer == shotStartDelay) {
+					//Shoot
+					shotStartTimer = 0;
+					Instantiate (bullet, transform.position, Quaternion.FromToRotation (Vector2.right, lookVector));
+					shotCounter = shotDelay;
+				}
 			}
 
-			coneAngle = expandedConeAngle;
-			coneLength = expandedConeLength;
+			//Shoot a bullet if it's looking at the player
+			if (IsPlayerInsideCone (lookVector, rad, coneLength)) {//is looking at the player
 
+				//Start the shot timer
+				if (shotCounter == 0) {
+
+					if (shotStartTimer == 0)
+						++shotStartTimer;
+					transform.FindChild ("Enemy Suspicion").gameObject.SetActive (false);
+				}
+
+				coneAngle = expandedConeAngle;
+				coneLength = expandedConeLength;
+
+			} else {
+
+				++timeOutOfRange;
+				if (timeOutOfRange >= extendedRangeTime) {
+					coneAngle = normalConeAngle;
+					coneLength = normalConeLength;
+					timeOutOfRange = 0;
+				}
+
+
+				transform.FindChild ("Enemy Suspicion").gameObject.SetActive (IsPlayerInsideCone (lookVector, sRad, suspiciousConeLength));
+			}
+
+			if (shotCounter > 0)
+				--shotCounter;
 		} else {
-
-            ++timeOutOfRange;
-            if(timeOutOfRange >= extendedRangeTime)
-            {
-                coneAngle = normalConeAngle;
-                coneLength = normalConeLength;
-            }
-			
-
-			transform.FindChild ("Enemy Suspicion").gameObject.SetActive (IsPlayerInsideCone (lookVector, sRad, suspiciousConeLength));
+			--dashHitTimer;
 		}
 
-		if(shotCounter > 0)
-			--shotCounter;
 	}
 
 	bool IsPlayerInsideCone(Vector2 lookVector, float coneAngleRad, float coneLen)
@@ -109,5 +122,11 @@ public class EnemyBehavior : MonoBehaviour {
 			Vector2.Dot ((Vector2.right * direction), lookVector.normalized) >= Mathf.Cos (coneAngleRad);
 	}
 
-   // bool OnCollision
+	void OnCollisionEnter2D(Collision2D clsn)
+	{
+		if (clsn.gameObject == player && playerAttack.dashing) {
+			dashHitTimer = dashHitTime;
+		}
+
+	}
 }
